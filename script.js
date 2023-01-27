aeb3f009b341a197003fa84e047d308cb93c680c
Jay <jay.pena1224@proton.me>
2023-01-27 13:06:10 -0500

    Change drawing function to require a left click

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const sketchPad = document.getElementById('sketchPad');
 let counter = 0;
 let keepGoing = true;
+let drawing = false;
+
+sketchPad.addEventListener('mousedown', () => {
+    drawing = true;
+});
+
+sketchPad.addEventListener('mouseup', () => {
+    drawing = false;
+});
 
 const draw = (el) => {
     el.classList.toggle('drawBlack')
 }
 
-while (keepGoing === true) {
-    if (counter === 16 * 16) {
-        keepGoing = false;
-    } else {
-        let div = document.createElement('div');
-        div.addEventListener('click', e => {
-            draw(e.target);
-        })
-        sketchPad.appendChild(div);
-        counter++;
+const createPad = function(num) {
+    sketchPad.removeAttribute('class');
+    sketchPad.classList.add(`size${num}`);
+    while (keepGoing === true) {
+        if (counter === num * num) {
+            keepGoing = false;
+        } else {
+            let div = document.createElement('div');
+            div.style.draggable = 'false';
+            div.addEventListener('mouseenter', e => {
+                if (drawing === true) {
+                    draw(e.target);
+                }
+            });
+            sketchPad.appendChild(div);
+            counter++;
+        }
     }
 }
+
+createPad(64);
